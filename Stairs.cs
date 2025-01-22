c308075ccff563c185871c3a93ccdbab502bcb69
robhowe-A <rhowell91@live.com>
2025-01-22 12:12:54 -0600

    stairs console output

---
+++ Stairs.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleStairs
+{
+    class Stairs
+    {
+        private static void WriteStairs()
+        {
+            double row = 0;
+            double j;
+            while (row < 10)
+            {
+                j = Math.Pow(row, 2);
+                for (int i=0; i <= j;i++)
+                {
+                    Console.Write("{0}", '*');
+                }
+                    Console.WriteLine();
+                row++;
+            }
+        }
+
+        static void Main()
+        {
+            WriteStairs();
+        }
+    };
+}
