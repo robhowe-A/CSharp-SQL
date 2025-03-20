fba08b4aaf7ae94d303f113c88655db0649a72f1
robhowe-A <rhowell91@live.com>
2025-03-20 11:44:15 -0500

    new idea

--- Polymorphism,Inheritance,Encapsulation,Abstraction/Program.cs
+++ Polymorphism,Inheritance,Encapsulation,Abstraction/Program.cs
@@ -1,22 +1,19 @@
 ﻿
-namespace Polymorphism_Inheritance_Encapsulation_Abstraction;
+namespace PIEA;
 
-internal class AreaApplication
+internal class ProductStore
 {
-    // Main class begins the program
-    private static int Main()
+    static int Main()
     {
-        // Call static method ShowInfo()
-        CalculateAreaStart.ShowInfo(["Circle", "Square"]);
+        CycleItem grips = new ("Strong grips", 45.00m, "BestBikesStuff", DateTime.Now.Date);
+        grips.ShowInfo();
+        grips.ShowInfo(true, true);
+        grips.ShowInfo(true, false);
+        grips.ShowInfo(false, true);
+        grips.ShowInfo(false, false);
 
-        // Instantiate and initialize new circle
-        Circle Circle3 = new (3.00);
-        Circle3.ShowInfo(true);
-
-        // Instantiate and initialize new circle
-        Square Square5 = new (5.00);
-        Square5.ShowInfo(true);
-
+        
+        
         return -1;
     }
-};
+}
